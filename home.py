53473c84296624c827adbbafb809f423c04c8963
hhb0 <131655783+hhb0@users.noreply.github.com>
2023-08-01 23:27:53 +0900

    Update

--- home.py
+++ home.py
@@ -14,6 +14,12 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 from streamlit import components
 import numpy as np
 import pandas as pd
